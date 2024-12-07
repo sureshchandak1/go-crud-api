53bfb1c65a0744258c89a3f9c8c8f06112f18d7d
SSureshC <sureshchandak1@gmail.com>
2024-12-07 19:19:55 +0530

    add get api

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/sureshchandak1/go-crud-api/internal/storage"
@@ -47,5 +48,30 @@ func New(storage storage.Storage) http.HandlerFunc {
 		slog.Info("user created successfully", slog.String("userId", fmt.Sprint(lastId)))
 
 		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
+	}
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		// get student from database
+		student, err := storage.GetStudentById(intId)
+
+		if err != nil {
+			slog.Error("error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
+
 	}
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,8 @@
 package storage
 
+import "github.com/sureshchandak1/go-crud-api/internal/types"
+
 type Storage interface {
 	CreateStudent(name, email string, age int) (int64, error)
+	GetStudentById(id int64) (types.Student, error)
 }
