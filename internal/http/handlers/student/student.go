c37f69dd209b0edc6b87ecb85680e0ccea35787e
SSureshC <sureshchandak1@gmail.com>
2024-12-07 19:31:11 +0530

    get list api

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -75,3 +75,19 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		slog.Info("getting all students")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+
+	}
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/sureshchandak1/go-crud-api/internal/types"
 type Storage interface {
 	CreateStudent(name, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
