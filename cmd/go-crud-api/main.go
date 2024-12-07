1c6ad2ed7609ef987400c9f481080bd42719cdea
SSureshC <sureshchandak1@gmail.com>
2024-12-07 17:40:23 +0530

    add request validation

--- cmd/go-crud-api/main.go
+++ cmd/go-crud-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/sureshchandak1/go-crud-api/internal/config"
+	"github.com/sureshchandak1/go-crud-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -22,9 +23,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
---
+++ internal/http/handlers/student/student.go
@@ -0,0 +1,41 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/sureshchandak1/go-crud-api/internal/types"
+	"github.com/sureshchandak1/go-crud-api/internal/utils/response"
+)
+
+func New() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("creating a student")
+
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		// request validation
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+	}
+}
