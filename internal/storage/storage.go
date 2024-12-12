797ef56191c517f8796e1d7fb0eb8127256c922b
SSureshC <sureshchandak1@gmail.com>
2024-12-12 12:32:29 +0530

    Connect PostgreSQL database

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,10 @@ package storage
 import "github.com/sureshchandak1/go-crud-api/internal/types"
 
 type Storage interface {
+	Student
+}
+
+type Student interface {
 	CreateStudent(name, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
 	GetStudents() ([]types.Student, error)
